9c482a6adaab24e210f1f816dd9b57ada727fa7d
Antonin-Landeau <alandeau@charles-poncet.net>
2023-05-15 08:44:16 +0200

    fix for deploy

--- components/Section/Projects/ProjectsHeader.tsx
+++ components/Section/Projects/ProjectsHeader.tsx
@@ -11,7 +11,7 @@ const ProjectsHeader = () => {
    <div className="z-10 p-5">
     <Title title="Mes Projets" className="relative z-10" variant="white" small />
     <p className="m-auto text-xl text-center pb-10 text-default-white max-w-2xl md:text-left relative z-10">
-     Retrouver tout les projet que j'ai pu réaliser au cours de mon cursus
+     Retrouver tout les projet que j&apos;ai pu réaliser au cours de mon cursus
      scolaire et de mon temps libre
     </p>
    </div>
